28e41ec45f504ee5842bdf2be6eb0bfb2c717250
ImraanMeyer <imraan.meyer97@gmail.com>
2019-07-29 11:30:13 +0200

    Updates

---
+++ precache-manifest.4b1c1c5b54b17d86acd91302f0cc7ec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424f9585753473016c137899c5ca0a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cee0d578e7ef039d4880",
+    "url": "/static/css/main.a46685d5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "cee0d578e7ef039d4880",
+    "url": "/static/js/main.637dea6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
